83186f94bf233c80a2df948e565ed0b38149915d
DanGrund <hello@dangrund.com>
2017-03-16 14:00:30 -0600

    links now work no cors issues

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const md5 = require('md5');
 const fs = require('fs');
 
+app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('src'));
@@ -98,7 +105,7 @@ app.get(`/:shortURL`, (request, response) => {
       }
     })
   })
-  response.redirect(`${longURL}`);
+  response.status(302).redirect(`${longURL}`);
 })
 
 
