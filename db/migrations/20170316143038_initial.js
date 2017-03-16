5d0120f026f5bec78d1d7d95f5c33081c7f3ddfe
DanGrund <hello@dangrund.com>
2017-03-16 14:38:44 -0600

    set up schema

---
+++ db/migrations/20170316143038_initial.js
@@ -0,0 +1,27 @@
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.createTable('folders', function(table) {
+            table.increments('id').primary();
+            table.string('name');
+            table.timestamps();
+        }),
+
+        knex.schema.createTable('urls', function(table){
+          table.increments('id').primary();
+          table.string('shortURL');
+          table.string('longURL');
+          table.integer('visitCount');
+          table.timestamps();
+          table.integer('folder_id')
+               .references('id')
+               .inTable('folders');
+        })
+    ])
+};
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTable('folders'),
+        knex.schema.dropTable('urls')
+    ])
+};
