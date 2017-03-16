ac53ec778954ee6c252162d364c7e87f91fb10ee
mlimberg <limbergmike@gmail.com>
2017-03-16 13:38:24 -0600

    Add test script to package.json

--- server.js
+++ server.js
@@ -100,7 +100,10 @@ app.get(`/:shortURL`, (request, response) => {
   response.redirect(`${longURL}`);
 })
 
+if(!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`The shit is lit AF over at ${app.get('port')}`);
+  })
+}
 
-app.listen(app.get('port'), () => {
-  console.log(`The shit is lit AF over at ${app.get('port')}`);
-})
+module.exports = app;
