7a7a1493bcb675b8d327eb5bd7923b18d92667f7
DanGrund <hello@dangrund.com>
2017-03-17 10:29:21 -0600

    add production db info

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: `postgres://cisgdxexbfscqj:b98d7bd1c7efd2e2b7bf37fbaaab0d656d16e59aef8ba154e8a79187c30f31a2@ec2-50-19-116-106.compute-1.amazonaws.com:5432/d7kp18oscj49kk` + `?ssl=true`,
     migrations: {
       directory: './db/migrations'
     },
